ff24637b1b58c75066047bcb51390d360a3d5e69
Niklas Alexander Shern <test@test.test>
2023-08-22 21:25:01 +0200

    test: add visualization test

--- tests/x_integrator_test.py
+++ tests/x_integrator_test.py
@@ -1,4 +1,5 @@
 from x_integrator.__main__ import Integration
+import plotly
 
 
 def test_create_integration():
@@ -10,3 +11,10 @@ def test_get_user_timeline():
     rihanna = Integration("rihanna")
     timeline = rihanna._get_user_timeline()
     assert timeline["meta"]["result_count"] > 0
+
+
+def test_visualization():
+    rihanna = Integration("rihanna")
+    rihanna.get_tweet_table(2)
+    fig = rihanna.visualize_popularity(rihanna.tweet_table)
+    assert isinstance(fig, plotly.graph_objs._figure.Figure)
