2a68f34d92e186f06686776085290071430a8a9f
Niklas Alexander Shern <test@test.test>
2023-08-22 15:38:50 +0200

    test: add test for user timeline

--- tests/x_integrator_test.py
+++ tests/x_integrator_test.py
@@ -4,3 +4,9 @@ from x_integrator.__main__ import Integration
 def test_create_integration():
     rihanna = Integration("rihanna")
     assert rihanna.user_id == "79293791"
+
+
+def test_get_user_timeline():
+    rihanna = Integration("rihanna")
+    timeline = rihanna._get_user_timeline()
+    assert timeline["meta"]["result_count"] > 0
