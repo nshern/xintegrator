e906399ba88f4494e516104904349365243822eb
Niklas Alexander Shern <test@test.test>
2023-09-01 14:13:06 +0200

    remove examples

--- examples/get_posts_as_html.py
+++
@@ -1,9 +0,0 @@
-from xintegrator import Integration
-
-riri = Integration("rihanna")
-
-riri.get_tweet_table(max_results=5, type="mentions")
-
-doc = riri.get_posts_as_embedded("mentions")
-
-print(doc)
